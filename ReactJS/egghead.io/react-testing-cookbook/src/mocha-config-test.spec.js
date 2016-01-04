28f7aa3d056941f3af90e5d92846159acc1f190a
Justin Wehrman <jwehrman@nerdery.com>
2016-01-04 13:15:50 -0600

    Add scripts/test config test file.

---
+++ ReactJS/egghead.io/react-testing-cookbook/src/mocha-config-test.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Simple test to ensure testing is configured correctly to use `npm test`
+ */
+import expect from 'expect';
+
+describe('Mocha Config Test', () => {
+
+    it('should work', () => {
+        expect(true).toEqual(true);
+    });
+
+});
