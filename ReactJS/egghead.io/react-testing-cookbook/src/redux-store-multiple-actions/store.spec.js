c49f7b967d5d311a2a0625c7f671d915a27178bd
Justin Wehrman <jwehrman@nerdery.com>
2016-01-04 17:00:29 -0600

    Starter for redux items

---
+++ ReactJS/egghead.io/react-testing-cookbook/src/redux-store-multiple-actions/store.spec.js
@@ -0,0 +1,54 @@
+/**
+ * Non-Functional Example
+ */
+
+import { store } from './store';
+import expect from 'expect';
+
+describe('store', () => {
+
+    it('should work with a series of actions', () => {
+        const actions = [
+            {
+                type: 'ADD_QUOTE_BY_ID',
+                payload: {
+                    text: 'Some text',
+                    author: 'Some author',
+                    id: 1,
+                    likeCount: 24
+                }
+            },
+            {
+                type: 'ADD_QUOTE_BY_ID',
+                payload: {
+                    text: 'Some other text',
+                    author: 'Some other author',
+                    id: 2,
+                    likeCount: 0
+                }
+            },
+            {
+                type: 'REMOVE_QUOTE_BY_ID',
+                payload: { id: 1 }
+            }
+        ];
+
+        actions.forEach(action => store.dispatch(action));
+
+        const actual = store.getState();
+        const expected = {
+            quotes: [
+                {
+                    text: 'Some other text',
+                    author: 'Some other author',
+                    id: 2,
+                    likeCount: 0
+                }
+            ]
+        };
+
+        expect(actual).toEqual(expected);
+
+    });
+
+});
