121091a6008f1da37d0a67677e46b91554c5ba63
Justin Wehrman <jwehrman@nerdery.com>
2016-01-04 15:07:53 -0600

    Adds jsx error in diffs using expect-jsx dependency.

---
+++ ReactJS/egghead.io/react-testing-cookbook/src/jsx-error-diffs/renderDollarValue.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import RenderDollarValue from './renderDollarValue';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+
+describe('RenderDollarValue', () => {
+
+    it('should return a dollar value in the form of `$#.##`', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<RenderDollarValue dollarValue='6.00' />);
+
+        // render method must be called before we can getRenderOutput()
+        const actual = renderer.getRenderOutput();
+        const expected = '$6.00';
+        const unexpected = '$$6.00';
+
+        expect(actual).toIncludeJSX(expected);
+        expect(actual).toNotIncludeJSX(unexpected);
+    });
+
+});
